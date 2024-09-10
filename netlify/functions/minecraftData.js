ac0329e870617db9c2aec40284ef1ffa12380512
unknow <138153020+ff6f8d68@users.noreply.github.com>
2024-09-10 11:53:09 -0700

    Update minecraftData.js

--- netlify/functions/minecraftData.js
+++ netlify/functions/minecraftData.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-exports.handler = async (event) => {
+export async function handler(event) {
     const username = event.queryStringParameters.username;
 
     if (!username) {
@@ -43,4 +43,4 @@ exports.handler = async (event) => {
             body: JSON.stringify({ error: error.message }),
         };
     }
-};
+}
